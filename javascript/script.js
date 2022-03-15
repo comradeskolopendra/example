5262a8a3222964075a62f49310b0214ef05dac7f
Pridanov Petr <p.pridanoff2016@yandex.kz>
2022-03-15 12:03:34 +0600

    fix js and css

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,31 @@
-const changeLinkState = document.querySelectorAll('.navbar__tab')
+const tabs = document.querySelectorAll('.navbar__tab')
 const variableForTest = document.querySelector('.container__main')
 
-changeLinkState.forEach(element => {
+tabs.forEach(element => {
     element.addEventListener('click', (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        changeLinkState.forEach(element => {
-            element.classList.remove('active')
-            element.classList.remove('text__focus_color')
+        tabs.forEach(element => {
             element.setAttribute('data-active', '0')
         })
-        element.classList.add('active');
-        element.classList.add('text__focus_color')
         element.setAttribute('data-active', '1')
     })
 })
 
-changeLinkState.forEach(element => {
-    element.addEventListener('mouseover', (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        element.classList.add('active');
-        element.classList.remove('inactive')
-        element.classList.add('text__focus_color')
-    })
-})
+// changeLinkState.forEach(element => {
+//     element.addEventListener('mouseover', (event) => {
+//         event.stopPropagation();
+//         event.preventDefault();
+//         element.classList.add('active');
+//         element.classList.remove('inactive')
+//         element.classList.add('text__focus_color')
+//     })
+// })
 
-changeLinkState.forEach(element => {
-    element.addEventListener('mouseout', (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        element.classList.remove('active');
-        element.classList.add('inactive')
-        element.classList.remove('text__focus_color')
-    })
-})
+// changeLinkState.forEach(element => {
+//     element.addEventListener('mouseout', (event) => {
+//         event.stopPropagation();
+//         event.preventDefault();
+//         element.classList.remove('active');
+//         element.classList.add('inactive')
+//         element.classList.remove('text__focus_color')
+//     })
+// })

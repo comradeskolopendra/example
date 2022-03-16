0b083a5b286a4fe0b2d06f2796b4bd85e7cfec1b
comradeskolopendra <91865240+comradeskolopendra@users.noreply.github.com>
2022-03-16 12:01:11 +0600

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,10 @@
 const tabs = document.querySelectorAll('.navbar__tab')
-const variableForTest = document.querySelector('.container__main')
 
 tabs.forEach(element => {
-    element.addEventListener('click', (event) => {
+    element.addEventListener('click', () => {
         tabs.forEach(element => {
             element.setAttribute('data-active', '0')
         })
         element.setAttribute('data-active', '1')
     })
 })
-
-// changeLinkState.forEach(element => {
-//     element.addEventListener('mouseover', (event) => {
-//         event.stopPropagation();
-//         event.preventDefault();
-//         element.classList.add('active');
-//         element.classList.remove('inactive')
-//         element.classList.add('text__focus_color')
-//     })
-// })
-
-// changeLinkState.forEach(element => {
-//     element.addEventListener('mouseout', (event) => {
-//         event.stopPropagation();
-//         event.preventDefault();
-//         element.classList.remove('active');
-//         element.classList.add('inactive')
-//         element.classList.remove('text__focus_color')
-//     })
-// })
